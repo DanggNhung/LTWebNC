e36748da474493b2a5d740bd325b657842f4bfe2
DangNhung <ngosipp@gmail.com>
2026-07-10 16:30:20 +0700

    sua lai db

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "L29**hg_N19*",
-    database: "student_management"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
-
-db.connect((err) => {
-    if (err) throw err;
+db.connect((err)=>{
+    if(err){
+        console.log("Database connection failed");
+        throw err;
+    }
     console.log("Connected MySQL");
 });
-
 module.exports = db;
